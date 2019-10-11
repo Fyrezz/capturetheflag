99c6254a8c848518c493e8321bd6c57890ba294d
Fyrezz <eduardo.sanchez.paz@gmail.com>
2019-10-11 16:13:52 +0200

    First

---
+++ CaptureTheFlag/src/main/java/net/fyrezz/capturetheflag/P.java
@@ -0,0 +1,17 @@
+package net.fyrezz.capturetheflag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class P extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+}
