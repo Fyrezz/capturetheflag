47c9f77b768991cf1d8a880a25dbccb587a61895
Fyrezz <eduardo.sanchez.paz@gmail.com>
2019-10-11 16:47:30 +0200

    Update

--- CaptureTheFlag/src/main/java/net/fyrezz/capturetheflag/P.java
+++ CaptureTheFlag/src/main/java/net/fyrezz/capturetheflag/P.java
@@ -1,11 +1,24 @@
 package net.fyrezz.capturetheflag;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class P extends JavaPlugin {
+	
+	private FileConfiguration config;
+	private FileConfiguration lang;
 
 	@Override
 	public void onEnable() {
+		// Make sure all files are loaded
+		saveResource("config.yml", false);
+		saveResource("lang.yml", false);
+		
+		this.config = YamlConfiguration.loadConfiguration(new File(this.getDataFolder(), "config.yml"));
+		this.lang = YamlConfiguration.loadConfiguration(new File(this.getDataFolder(), "lang.yml"));
 
 	}
 
@@ -13,5 +26,13 @@ public class P extends JavaPlugin {
 	public void onDisable() {
 
 	}
-
+	
+	public FileConfiguration getConfig() {
+		return config;
+	}
+	
+	public FileConfiguration getLang() {
+		return lang;
+	}
+	
 }
